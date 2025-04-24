07478583074fe7ee6b8255eabf33d5d9938b3ba6
kyum2n <aphrod207@gmail.com>
2025-04-25 02:47:38 +0900

    googleLogin

--- src/main/java/com/example/webapp/entity/User.java
+++ src/main/java/com/example/webapp/entity/User.java
@@ -1,8 +1,12 @@
 package com.example.webapp.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor // 기본 생성자
+@AllArgsConstructor // 모든 필드 생성자
 public class User {
     private String uName;
     private String uId;
